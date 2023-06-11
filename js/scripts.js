bf336f9699a7cdd0b7951dba49c96492b0be7a84
Jase Grable <jase.grable@gmail.com>
2023-06-10 18:38:04 -0700

    Update reverseNumber function with unique variable names, added new test to README

--- js/scripts.js
+++ js/scripts.js
@@ -2,27 +2,6 @@
 let form = document.getElementById("numberCountForm")
 let number = form.number.value; 
 
-
-// function numberCount(number) {
-//     let array = [];
-//     for (let i = 0; i <= number; i++) {
-//         if (String(i).includes("1") || String(i).includes("2") || String(i).includes("3")) {
-//             if (String(i).includes("1")) {
-//                 array.push("Beep!");
-//             } else if (String(i).includes("2")) {
-//                 array.push("boop!");
-//             } else {
-//                 array.push("won't you be my neighbor");
-//             }
-//         } else {
-//             array.push(i);
-//         }
-//     }
-//     return array;
-// }
-
-
-
 function numberCount(number) {
     let array = [];
     for (let i = 0; i <= number; i++) {
@@ -42,11 +21,12 @@ function numberCount(number) {
 
 
   function reverseNumber(number) {
-    let array = [];
+    let arrayReverse = [];
     for (let i = number; i >= 0; i--) {
-      let str = String(i);
-      let newStr = str;
-     array.push(newStr);
+      let strReverse = String(i);
+      let newStrReverse = strReverse;
+     array.push(newStrReverse);
     }
     return array;
   }
+
